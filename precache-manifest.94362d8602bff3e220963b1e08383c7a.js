7d5a698d22650dbb977904cd5abbe74f51353a51
mateusriff <mateusnriff@gmail.com>
2023-01-20 19:12:59 -0300

    Updates

---
+++ precache-manifest.94362d8602bff3e220963b1e08383c7a.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fef2d144b8241139689692b4f54b743b",
+    "url": "/Keeper/index.html"
+  },
+  {
+    "revision": "2ec50e8d04ad6520f3e0",
+    "url": "/Keeper/static/js/2.86d630e7.chunk.js"
+  },
+  {
+    "revision": "7caefff58a5e7bc4e7d9",
+    "url": "/Keeper/static/js/main.2a184aa9.chunk.js"
+  },
+  {
+    "revision": "46e90fe1cfd8c06e01df",
+    "url": "/Keeper/static/js/runtime-main.f2e2a15a.js"
+  }
+]);
